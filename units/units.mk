7c35bb25f8d8eae96468238605b21a164c2185bf
Quentin Glidic <sardemff7+git@sardemff7.net>
2016-04-21 12:29:10 +0200

    webhook/systemd: Install as a system unit
    Signed-off-by: Quentin Glidic <sardemff7+git@sardemff7.net>

--- units/units.mk
+++ units/units.mk
@@ -1,12 +1,12 @@
 EXTRA_DIST += \
-	$(systemduserunit_DATA:=.in) \
+	$(systemdsystemunit_DATA:=.in) \
 	$(null)
 
 CLEANFILES += \
-	$(systemduserunit_DATA) \
+	$(systemdsystemunit_DATA) \
 	$(null)
 
-$(systemduserunit_DATA): %: %.in $(CONFIG_HEADER) %D%/units.mk
+$(systemdsystemunit_DATA): %: %.in $(CONFIG_HEADER) %D%/units.mk
 	$(AM_V_GEN)$(MKDIR_P) $(dir $@) && \
 		$(SED) \
 		-e 's:[@]bindir[@]:$(bindir):g' \
