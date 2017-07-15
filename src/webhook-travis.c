8be13bc8c461569ae711330bf1ae901b4295e43a
Quentin Glidic <sardemff7+git@sardemff7.net>
2017-07-15 09:27:54 +0200

    webhook/travis: Use more event-like names
    Signed-off-by: Quentin Glidic <sardemff7+git@sardemff7.net>

--- src/webhook-travis.c
+++ src/webhook-travis.c
@@ -30,13 +30,36 @@
 
 #include <json-glib/json-glib.h>
 
+#include "nkutils-enum.h"
 #include "libgit-eventc.h"
 #include "webhook-travis.h"
+
+enum {
+    GIT_EVENTC_WEBHOOK_TRAVIS_STATE_PASSED,
+    GIT_EVENTC_WEBHOOK_TRAVIS_STATE_FAILED,
+    GIT_EVENTC_WEBHOOK_TRAVIS_STATE_ERRORED,
+};
+
+static const gchar * const _git_eventc_webhook_travis_state_name[] = {
+    [GIT_EVENTC_WEBHOOK_TRAVIS_STATE_PASSED]  = "passed",
+    [GIT_EVENTC_WEBHOOK_TRAVIS_STATE_FAILED]  = "failed",
+    [GIT_EVENTC_WEBHOOK_TRAVIS_STATE_ERRORED] = "errored",
+};
+
+static const gchar * const _git_eventc_webhook_travis_state_actions[] = {
+    [GIT_EVENTC_WEBHOOK_TRAVIS_STATE_PASSED]  = "success",
+    [GIT_EVENTC_WEBHOOK_TRAVIS_STATE_FAILED]  = "failure",
+    [GIT_EVENTC_WEBHOOK_TRAVIS_STATE_ERRORED] = "error",
+};
 
 void
 git_eventc_webhook_payload_parse_travis(const gchar **project, JsonObject *root)
 {
-    const gchar *action = json_object_get_string_member(root, "state");
+    const gchar *state = json_object_get_string_member(root, "state");
+    guint64 action;
+
+    if ( ! nk_enum_parse(state, _git_eventc_webhook_travis_state_name, G_N_ELEMENTS(_git_eventc_webhook_travis_state_name), TRUE, FALSE, &action) )
+        return;
 
     const gchar *number_str = json_object_get_string_member(root, "number");
     gchar *e;
@@ -57,11 +80,11 @@ git_eventc_webhook_payload_parse_travis(const gchar **project, JsonObject *root)
     url = git_eventc_get_url_const(json_object_get_string_member(root, "build_url"));
 
     if ( json_object_get_boolean_member(root, "pull_request") )
-        git_eventc_send_ci_build_for_pull_request(action, number, branch, duration,
+        git_eventc_send_ci_build_for_pull_request(_git_eventc_webhook_travis_state_actions[action], number, branch, duration,
             json_object_get_int_member(root, "pull_request_number"),
             json_object_get_string_member(root, "pull_request_title"),
             git_eventc_get_url_const(json_object_get_string_member(root, "compare_url")),
             url, repository_name, repository_url, project);
     else
-        git_eventc_send_ci_build(action, number, branch, duration, url, repository_name, repository_url, project);
+        git_eventc_send_ci_build(_git_eventc_webhook_travis_state_actions[action], number, branch, duration, url, repository_name, repository_url, project);
 }
